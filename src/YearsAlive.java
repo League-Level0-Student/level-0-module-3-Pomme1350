4d245b977668236e75d648e283fe6efd5d8eaca6
League <League@administorsimac.attlocal.net>
2019-02-16 11:28:52 -0800

    eclipse in makeup lab

--- src/YearsAlive.java
+++ src/YearsAlive.java
@@ -1,4 +1,11 @@
+import javax.swing.JOptionPane;
 
 public class YearsAlive {
-
+	public static void main(String[] args) {
+		String year = JOptionPane.showInputDialog(null, "What year were you born?");
+		JOptionPane.showMessageDialog(null, "You are"  );
+		
+		
+	
+	}
 }
